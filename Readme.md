397eb9a4ada6fcc41028cde089ffedf8fedcd0ab
Gaurav <epicgaurav554@gmail.com>
2026-03-07 00:53:00 +0530

    Readme File

---
+++ Readme.md
@@ -0,0 +1,33 @@
+# This code is only made purpose for auto drop in private channel ... this is the way  exploite to  the bot.
+# but you can do it... Until you get catch/reported or get blacklisted from bot....
+
+
+
+# If you are running this file in your personal device to make sure install some mandatory packages...
+1. seflbot package, simply type in terminal also you can add version which is the last version btw ...(Big shotout to Akeno who made this package of selfbot...)
+`npm install discord.js-selfbot-v13`
+2. Chalk file which make your console text beautiful this has a specific persion which is mention in `package.json((chalk": "^4.1.2")) file just copy and type in terminal 
+`npm install chalk@4.1.2`
+
+
+# And all done for package install now put your discord account info in defined file thing like put ur token without ("") cots in token.txt file & one channel id in channel.txt file
+
+
+
+
+# And all set .
+# You are good to go ........
+
+# To run the file type : `node index.js`
+# Because index is the main coding file.
+
+
+
+# This is Unwanted info ...... you can read for waste your time
+
+# This code is not well optimized coz this work for 3-5bot, that's why it's not well optimized, it consume high cpu and high ram while running the code. You can improve the code as per your requirement of which bot you are doing that 
+
+
+`Made By "EPIC G" `
+
+THANK YOU VERY MUCH FOR READ ALL.
---
+++ index.js
@@ -0,0 +1,117 @@
+// prettier-ignore
+const chalk = require('chalk');
+const { Client } = require("discord.js-selfbot-v13");
+const fs = require('fs');
+
+//Channel & tokens read karne ka area
+const channelId = fs.readFileSync('channel.txt', 'utf-8').trim();
+const tokens = fs.readFileSync('token.txt', 'utf-8').trim().split('\n').map(t => t.trim()).filter(t => t.length > 0);
+
+
+function timeDelay(min, max) {
+    const ms = Math.floor(Math.random() * (max - min + 1) + min);
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+//Main Area from starting............... lessgoooooooooooooooooooo
+tokens.forEach(async(token, index) => {
+    const client = new Client({ checkUpdate: false });
+
+
+    client.on('ready', async(msg) => {
+        console.log(chalk.black.bgGreen(`I am Online ${client.user.username}`));
+        const channel = await client.channels.fetch(channelId);
+        if (!channel) return;
+        if (!channel.length === 0) return;
+
+
+
+        const sdMessage = async() => {
+            try {
+                await channel.send('ns'); // Command which you want to send 'kd'
+                console.log(chalk.gray(`Ns sent at ${new Date().toLocaleTimeString()}`));
+
+                const nextDelay = Math.floor(Math.random() * (780000 - 660000 + 1) + 660000);
+                console.log(chalk.green(`Next Ns in ${Math.round(nextDelay/60000)} minutes:|`));
+                setTimeout(sdMessage, nextDelay);
+
+            } catch (error) {
+                console.log(chalk.red(`Failed to send message ${error}`));
+                setTimeout(sdMessage, 60000);
+            }
+        };
+
+        async function dailyTask() {
+            return await channel.send("ndaily");
+        }
+
+
+        // Area Message drop Duration adjustment 
+        setTimeout(async() => {
+            await timeDelay(2000, 3000);
+            await sdMessage();
+        }, 0);
+
+        //for Daily Task things drop   using Error-Handling conecpt to reduce crashing
+        setTimeout(async() => {
+            try {
+                await dailyTask();
+                console.log(chalk.magentaBright(`Daily Task sended ${new Date().toLocaleTimeString()}`));
+                await timeDelay(20000, 50000);
+
+                setInterval(async() => {
+                    await dailyTask();
+                }, 86400000)
+                console.log(chalk.magentaBright(`Daily task Sended`));
+
+            } catch (error) {
+                console.log(chalk.redBright(`Daily Task Error ===>  ${error.message}`));
+            }
+        }, 10000 * index)
+
+    });
+
+
+    client.on('messageCreate', async(message) => {
+        try {
+            //There is Bot, you can change as per need (Nai bot id) work for Sofi, gachpon, Tofu.... and also maybe for karuta
+            const botId = "1312830013573169252";
+            if (message.channel.id !== channelId) return;
+            if (message.author.id === botId) {
+                console.log(chalk.green(`This is ${message.author.username} Bot`));
+
+                if (message.attachments.size > 0 || message.components.length > 0) {
+                    console.log(chalk.cyan.bold(`${message.author.username} Card Droping`));
+                    try {
+                        const firstRow = message.components[0];
+                        const buttons = firstRow.components;
+
+                        if (buttons) {
+                            let randomIndex = Math.floor(Math.random() * buttons.length);
+                            let targetButton = message.components[0].components[randomIndex];
+                            //Duration gap to click button you can change per as need
+                            await timeDelay(2000, 3000);
+                            const myId = targetButton.customId; // this is where i got stucked once:)
+                            await message.clickButton(myId);
+                            console.log(chalk.hex('#FFA500').bold(`Button is click`));
+                        }
+                    } catch (error) {
+                        console.log(chalk.redBright(`Gaurav, There is error in Button = ${error.message}`));
+
+                    }
+                }
+            }
+        } catch (error) {
+            console.log(chalk.redBright(`There is a error in messageCreate function ===> ${error.message}`));
+        }
+
+    });
+
+    setTimeout(async() => {
+        client.login(token);
+    }, 55000 * index);
+
+});
+//if button not clicked this will trigger for not crashing the process  if catch is not define
+process.on("unhandledRejection", () => {});
+
+process.on("uncaughtException", () => {});
